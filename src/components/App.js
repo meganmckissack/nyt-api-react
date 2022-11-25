482faef2e81ccc5cfbfec5194fbd27cccb9af813
Megan McKissack <meganmckissack@gmail.com>
2022-11-25 14:00:07 -0800

    create topstories api call and response -wip

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import './App.css';
+import TopStories from './TopStories';
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="App">
+        <TopStories />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
